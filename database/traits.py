f537455b1b38e821b50a6eb971206d51aa184703
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-06-15 09:57:03 +0300

    Add blades

--- database/traits.py
+++ database/traits.py
@@ -54,6 +54,16 @@ HardLaunch = Item(
 
 # endregion
 
+# region Blades
+
+EnduringBlade = Item(name='Enduring Blade', hash=1261178282)
+HungryEdge = Item(name='Hungry Edge', hash=436053704)
+TemperedEdge = Item(name='Tempered Edge', hash=938542991)
+HonedEdge = Item(name='Honed Edge', hash=2827049491)
+JaggedEdge = Item(name='Jagged Edge', hash=3666208348)
+
+# endregion
+
 # region Magazines
 
 TacticalMag = Item(
