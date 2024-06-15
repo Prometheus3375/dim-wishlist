45d7375c6d60bc23675f7f8782b5448130f12be3
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-06-15 10:07:49 +0300

    [db.items] Add Ergo Sum

--- database/items.py
+++ database/items.py
@@ -2,8 +2,6 @@ from classes import Item
 
 AnyItem = Item(name='DIM Wildcard', hash=-69420)
 
-# region Weapons
-
 # region Omolon
 
 YarovitMG4 = Item(
@@ -20,6 +18,34 @@ Suspectum4FR = Item(
     name='Suspectum-4FR',
     hash=3615421669,
     icon='https://bungie.net/common/destiny2_content/icons/70bea113574eff7b0211a4f28f54b7a3.jpg',
+    )
+
+# endregion
+
+# region The Pale Heart
+
+ErgoSum = Item(
+    name='Ergo Sum',
+    hash=1681583613,
+    icon='https://bungie.net/common/destiny2_content/icons/39a94c98eba4b9e007a76bbc067752f7.jpg',
+    )
+
+Stoicism = Item(
+    name='Stoicism',
+    hash=266021826,
+    icon='https://bungie.net/common/destiny2_content/icons/04b8c22e60222cf4b19d3183030bd840.jpg',
+    )
+
+Relativism = Item(
+    name='Relativism',
+    hash=2809120022,
+    icon='https://bungie.net/common/destiny2_content/icons/17daeef6743335a6b5c283c1bed8b866.jpg',
+    )
+
+Solipsism = Item(
+    name='Solipsism',
+    hash=2273643087,
+    icon='https://bungie.net/common/destiny2_content/icons/8463509172359afb2f03f470b44f4c6f.jpg',
     )
 
 # endregion
@@ -54,29 +80,6 @@ LineInTheSand = Item(
     name='Line in the Sand',
     hash=2450049485,
     icon='https://bungie.net/common/destiny2_content/icons/f3cbf93ad663a4167acccff27e661bae.jpg',
-    )
-
-# endregion
-
-# endregion
-
-# region Armor
-Stoicism = Item(
-    name='Stoicism',
-    hash=266021826,
-    icon='https://bungie.net/common/destiny2_content/icons/04b8c22e60222cf4b19d3183030bd840.jpg',
-    )
-
-Relativism = Item(
-    name='Relativism',
-    hash=2809120022,
-    icon='https://bungie.net/common/destiny2_content/icons/17daeef6743335a6b5c283c1bed8b866.jpg',
-    )
-
-Solipsism = Item(
-    name='Solipsism',
-    hash=2273643087,
-    icon='https://bungie.net/common/destiny2_content/icons/8463509172359afb2f03f470b44f4c6f.jpg',
     )
 
 # endregion
