0b2a5ecb61867370fbe149f8af90205f34cad023
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-06-15 03:55:22 +0300

    Add item database

---
+++ database/items.py
@@ -0,0 +1,84 @@
+from classes import Item
+
+AnyItem = Item(name='DIM Wildcard', hash=-69420)
+
+# region Weapons
+
+# region Omolon
+
+YarovitMG4 = Item(
+    name='Yarovit MG4',
+    hash=3959549446,
+    icon='https://bungie.net/common/destiny2_content/icons/eb36bfdbaf6f2a50529a4c8a0b6bc281.jpg',
+    )
+
+# endregion
+
+# region Veist
+
+Suspectum4FR = Item(
+    name='Suspectum-4FR',
+    hash=3615421669,
+    icon='https://bungie.net/common/destiny2_content/icons/70bea113574eff7b0211a4f28f54b7a3.jpg',
+    )
+
+# endregion
+
+# region Echoes
+
+Breachlight = Item(
+    name='Breachlight',
+    hash=2328923181,
+    icon='https://bungie.net/common/destiny2_content/icons/eb5d014d193dbb12cac96c0c483796a5.jpg',
+    )
+
+PatronOfLostCauses = Item(
+    name='Patron of Lost Causes',
+    hash=2249996761,
+    icon='https://bungie.net/common/destiny2_content/icons/e7edef5ef70f6fb41034bf557f7b8feb.jpg',
+    )
+
+PerfectParadox = Item(
+    name='Perfect Paradox',
+    hash=1298672084,
+    icon='https://bungie.net/common/destiny2_content/icons/fb27afcf6cb34ca3ef2540af6b87c034.jpg',
+    )
+
+MartyrsRetribution = Item(
+    name="Martyr's Retribution",
+    hash=2584830733,
+    icon='https://bungie.net/common/destiny2_content/icons/39e43b57894237fdc40af4aeb1fe0fa3.jpg',
+    )
+
+LineInTheSand = Item(
+    name='Line in the Sand',
+    hash=2450049485,
+    icon='https://bungie.net/common/destiny2_content/icons/f3cbf93ad663a4167acccff27e661bae.jpg',
+    )
+
+# endregion
+
+# endregion
+
+# region Armor
+Stoicism = Item(
+    name='Stoicism',
+    hash=266021826,
+    icon='https://bungie.net/common/destiny2_content/icons/04b8c22e60222cf4b19d3183030bd840.jpg',
+    )
+
+Relativism = Item(
+    name='Relativism',
+    hash=2809120022,
+    icon='https://bungie.net/common/destiny2_content/icons/17daeef6743335a6b5c283c1bed8b866.jpg',
+    )
+
+Solipsism = Item(
+    name='Solipsism',
+    hash=2273643087,
+    icon='https://bungie.net/common/destiny2_content/icons/8463509172359afb2f03f470b44f4c6f.jpg',
+    )
+
+# endregion
+
+del Item
