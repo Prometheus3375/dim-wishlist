212d1d2aa8ddb37fb4ad12073b56fd7b44f0b7d7
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-06-16 04:45:41 +0300

    [database] Change structure and remove trait icons

---
+++ database/magazines.py
@@ -0,0 +1,6 @@
+from classes import Item
+
+TacticalMag = Item(name='Tactical Mag', hash=106909392)
+HighVelocityRounds = Item(name='High-Velocity Rounds', hash=2822142346)
+
+del Item
--- database/traits.py
+++ database/traits.py
@@ -1,313 +1,37 @@
 from classes import Item
 
-# region Barrels
-
-Smallbore = Item(
-    name='Smallbore',
-    hash=1482024992,
-    icon='https://bungie.net/common/destiny2_content/icons/7d1bb6a623f14959a47156ee71acfc3a.png',
-    )
-
-BarrelShroud = Item(
-    name='Barrel Shroud',
-    hash=1300023272,
-    icon='https://bungie.net/common/destiny2_content/icons/d9e5e8fa07a84c29252b78b0e7b3106d.png',
-    )
-
-CorkscrewRifling = Item(
-    name='Corkscrew Rifling',
-    hash=4090651448,
-    icon='https://bungie.net/common/destiny2_content/icons/7730e1732ed7ef91afce50277639c493.png',
-    )
-
-ArrowheadBrake = Item(
-    name='Arrowhead Brake',
-    hash=839105230,
-    icon='https://bungie.net/common/destiny2_content/icons/8f7fb54687aa52a9c2595d90ce9c9417.png',
-    )
-
-# endregion
-
-# region Scopes
-
-# endregion
-
-# region Launcher Barrels
-
-VolatileLaunch = Item(
-    name='Volatile Launch',
-    hash=1478423395,
-    icon='https://bungie.net/common/destiny2_content/icons/d255c9140cf640cfc820f995d1c1e96a.png',
-    )
-
-QuickLaunch = Item(
-    name='Quick Launch',
-    hash=3525010810,
-    icon='https://bungie.net/common/destiny2_content/icons/dfad5d8b00b79c4393943b791a624874.png',
-    )
-
-HardLaunch = Item(
-    name='Hard Launch',
-    hash=981914802,
-    icon='https://bungie.net/common/destiny2_content/icons/8bedf618841b334417b8f95c2aa35d57.png',
-    )
-
-# endregion
-
-# region Blades
-
-EnduringBlade = Item(name='Enduring Blade', hash=1261178282)
-HungryEdge = Item(name='Hungry Edge', hash=436053704)
-TemperedEdge = Item(name='Tempered Edge', hash=938542991)
-HonedEdge = Item(name='Honed Edge', hash=2827049491)
-JaggedEdge = Item(name='Jagged Edge', hash=3666208348)
-
-# endregion
-
-# region Magazines
-
-TacticalMag = Item(
-    name='Tactical Mag',
-    hash=106909392,
-    icon='https://bungie.net/common/destiny2_content/icons/f0e309a267b25279a871d182f8910bcc.png',
-    )
-
-HighVelocityRounds = Item(
-    name='High-Velocity Rounds',
-    hash=2822142346,
-    icon='https://bungie.net/common/destiny2_content/icons/2a2b757cd73305126990cd19ad011a2c.png',
-    )
-
-# endregion
-
-# region Batteries
-
-# endregion
-
-# region Traits
-
-Demolitionist = Item(
-    name='Demolitionist',
-    hash=3523296417,
-    icon='https://bungie.net/common/destiny2_content/icons/da009697110e06b65d6f827b3b6d5f70.png',
-    )
-
-Pugilist = Item(
-    name='Pugilist',
-    hash=691659142,
-    icon='https://bungie.net/common/destiny2_content/icons/93bb6bb34fb0616805ba2a7181ab386c.png',
-    )
-
-ThreatDetector = Item(
-    name='Threat Detector',
-    hash=4071163871,
-    icon='https://bungie.net/common/destiny2_content/icons/9611bdbc48aff898193fd5f705c7cf30.png',
-    )
-
-DesperateMeasures = Item(
-    name='Desperate Measures',
-    hash=2109543898,
-    icon='https://bungie.net/common/destiny2_content/icons/25692d31f4963c07c3a82aba508b52b6.png',
-    )
-
-AdrenalineJunkie = Item(
-    name='Adrenaline Junkie',
-    hash=11612903,
-    icon='https://bungie.net/common/destiny2_content/icons/6f8bde03140b53e6dc583ada1aeaa51d.png',
-    )
-
-Swashbuckler = Item(
-    name='Swashbuckler',
-    hash=4082225868,
-    icon='https://bungie.net/common/destiny2_content/icons/42631a4aff4432e794529a5a707eba61.png',
-    )
-
-VorpalWeapon = Item(
-    name='Vorpal Weapon',
-    hash=1546637391,
-    icon='https://bungie.net/common/destiny2_content/icons/bc5130b227ee577f2678ec2d2c97bf4a.png',
-    )
-
-Hatchling = Item(
-    name='Hatchling',
-    hash=831391274,
-    icon='https://bungie.net/common/destiny2_content/icons/2f7e898e2231900ac968ffb2080fc02b.png',
-    )
-
-Slice = Item(
-    name='Slice',
-    hash=923806249,
-    icon='https://bungie.net/common/destiny2_content/icons/1874354fc06817cf75a451d2ad1419e1.png',
-    )
-
-TripleTap = Item(
-    name='Triple Tap',
-    hash=3400784728,
-    icon='https://bungie.net/common/destiny2_content/icons/0795f44c0c3611b15428742c67918cd1.png',
-    )
-
-RapidHit = Item(
-    name='Rapid Hit',
-    hash=247725512,
-    icon='https://bungie.net/common/destiny2_content/icons/de22847fa94634a487758041887bcdaa.png',
-    )
-
-Strategist = Item(
-    name='Strategist',
-    hash=880644845,
-    icon='https://bungie.net/common/destiny2_content/icons/dc9057be28071222ed303d6baab3eb4f.png',
-    )
-
-ExplosivePayload = Item(
-    name='Explosive Payload',
-    hash=3038247973,
-    icon='https://bungie.net/common/destiny2_content/icons/31f02e5f3bae68ecdfdd3f899cf931c7.png',
-    )
-
-KineticTremors = Item(
-    name='Kinetic Tremors',
-    hash=3891536761,
-    icon='https://bungie.net/common/destiny2_content/icons/300c343e6c33651a74d8a3fe81536146.png',
-    )
-
-PrecisionInstrument = Item(
-    name='Precision Instrument',
-    hash=555281244,
-    icon='https://bungie.net/common/destiny2_content/icons/e6a4ebc1a8a73e4c7fcfc21c69000ce2.png',
-    )
-
-DualLoader = Item(
-    name='Dual Loader',
-    hash=25606670,
-    icon='https://bungie.net/common/destiny2_content/icons/628a238007fc97c4d4a85d0cd6933dba.png',
-    )
-
-TrenchBarrel = Item(
-    name='Trench Barrel',
-    hash=2360754333,
-    icon='https://bungie.net/common/destiny2_content/icons/44f1e50b0d6629fc2f08bc7067ce011b.png',
-    )
-
-OneTwoPunch = Item(
-    name='One-Two Punch',
-    hash=2679249093,
-    icon='https://bungie.net/common/destiny2_content/icons/013a56db7dc9c3f89012a381f601a4ff.png',
-    )
-
-FieldPrep = Item(
-    name='Field Prep',
-    hash=2869569095,
-    icon='https://bungie.net/common/destiny2_content/icons/4ee1ecaead6fe2de1a2cab07f8b7be3f.png',
-    )
-
-HealClip = Item(
-    name='Heal Clip',
-    hash=3017780555,
-    icon='https://bungie.net/common/destiny2_content/icons/3d0f2eb4c1e4f66f2bd897ec4d5ce4f1.png',
-    )
-
-KillClip = Item(
-    name='Kill Clip',
-    hash=1015611457,
-    icon='https://bungie.net/common/destiny2_content/icons/0afd4799427004b2d8170ab213a2ef0c.png',
-    )
-
-Incandescent = Item(
-    name='Incandescent',
-    hash=4293542123,
-    icon='https://bungie.net/common/destiny2_content/icons/22ed4a2cd5a0eb129a14ce77db49373f.png',
-    )
-
-AutoLoadingHolster = Item(
-    name='Auto-Loading Holster',
-    hash=3300816228,
-    icon='https://bungie.net/common/destiny2_content/icons/e4262099a1c2cea78b48a6680c5248c9.png',
-    )
-
-EnviousAssassin = Item(
-    name='Envious Assassin',
-    hash=968510818,
-    icon='https://bungie.net/common/destiny2_content/icons/bf7fccf6de8ee6d2e9e6285c004a4758.png',
-    )
-
-Voltshot = Item(
-    name='Voltshot',
-    hash=2173046394,
-    icon='https://bungie.net/common/destiny2_content/icons/a71ffb1cb221ea64fa77ab473f8a3a61.png',
-    )
-
-BaitAndSwitch = Item(
-    name='Bait and Switch',
-    hash=3078487919,
-    icon='https://bungie.net/common/destiny2_content/icons/bc91e95edfbc9163dd88339a93b66913.png',
-    )
-
-FiringLine = Item(
-    name='Firing Line',
-    hash=1771339417,
-    icon='https://bungie.net/common/destiny2_content/icons/92a5e8dbef32a414709d3196cb815d89.png',
-    )
-
-ClownCartridge = Item(
-    name='Clown Cartridge',
-    hash=2284787283,
-    icon='https://bungie.net/common/destiny2_content/icons/794d65813703324c653878def082ca66.png',
-    )
-
-EnlightenedAction = Item(
-    name='Enlightened Action',
-    hash=3828510309,
-    icon='https://bungie.net/common/destiny2_content/icons/cee13d00e65346dacc05562c80f537da.png',
-    )
-
-RewindRounds = Item(
-    name='Rewind Rounds',
-    hash=3418782618,
-    icon='https://bungie.net/common/destiny2_content/icons/a201fd61905b802867b677cf927e6e04.png',
-    )
-
-Surrounded = Item(
-    name='Surrounded',
-    hash=3708227201,
-    icon='https://bungie.net/common/destiny2_content/icons/979881b68ca7839267c9aef5aea2b0ce.png',
-    )
-
-HighGround = Item(
-    name='High Ground',
-    hash=2319119708,
-    icon='https://bungie.net/common/destiny2_content/icons/5ee6f65d7af0a9acf6f22e3254e85283.png',
-    )
-
-FourthTimesTheCharm = Item(
-    name="Fourth Time's the Charm",
-    hash=1354429876,
-    icon='https://bungie.net/common/destiny2_content/icons/a5edd4e751e1aa34bf8328a63b121e71.png',
-    )
-
-# endregion
-
-# region Origin Traits
-
-# endregion
-
-# region Ergo Sum
-
-ES_WolfpackRounds = Item(name='Wolfpack Rounds', hash=1959135343)
-ES_GatheringLight = Item(name='Gathering Light', hash=3551049142)
-ES_SacredFlame = Item(name='Sacred Flame', hash=3034060702)
-ES_ThePerfectFifth = Item(name='The Perfect Fifth', hash=1522566605)
-ES_ArcConductor = Item(name='Arc Conductor', hash=3949068808)
-ES_Stormbringer = Item(name='Stormbringer', hash=461437088)
-ES_UnplannedReprieve = Item(name='Unplanned Reprieve', hash=1327961177)
-ES_InsectoidRobotGrenades = Item(name='Insectoid Robot Grenades', hash=3325420353)
-
-ES_VortexFrame = Item(name='Vortex Frame', hash=2557881346)
-ES_CasterFrame = Item(name='Caster Frame', hash=519046634)
-ES_WaveSwordFrame = Item(name='Wave Sword Frame', hash=289811733)
-ES_LightweightFrame = Item(name='Lightweight Frame', hash=2753228730)
-ES_AggressiveFrame = Item(name='Aggressive Frame', hash=1726251414)
-
-# endregion
-
+Demolitionist = Item(name='Demolitionist', hash=3523296417)
+Pugilist = Item(name='Pugilist', hash=691659142)
+ThreatDetector = Item(name='Threat Detector', hash=4071163871)
+DesperateMeasures = Item(name='Desperate Measures', hash=2109543898)
+AdrenalineJunkie = Item(name='Adrenaline Junkie', hash=11612903)
+Swashbuckler = Item(name='Swashbuckler', hash=4082225868)
+VorpalWeapon = Item(name='Vorpal Weapon', hash=1546637391)
+Hatchling = Item(name='Hatchling', hash=831391274)
+Slice = Item(name='Slice', hash=923806249)
+TripleTap = Item(name='Triple Tap', hash=3400784728)
+RapidHit = Item(name='Rapid Hit', hash=247725512)
+Strategist = Item(name='Strategist', hash=880644845)
+ExplosivePayload = Item(name='Explosive Payload', hash=3038247973)
+KineticTremors = Item(name='Kinetic Tremors', hash=3891536761)
+PrecisionInstrument = Item(name='Precision Instrument', hash=555281244)
+DualLoader = Item(name='Dual Loader', hash=25606670)
+TrenchBarrel = Item(name='Trench Barrel', hash=2360754333)
+OneTwoPunch = Item(name='One-Two Punch', hash=2679249093)
+FieldPrep = Item(name='Field Prep', hash=2869569095)
+HealClip = Item(name='Heal Clip', hash=3017780555)
+KillClip = Item(name='Kill Clip', hash=1015611457)
+Incandescent = Item(name='Incandescent', hash=4293542123)
+AutoLoadingHolster = Item(name='Auto-Loading Holster', hash=3300816228)
+EnviousAssassin = Item(name='Envious Assassin', hash=968510818)
+Voltshot = Item(name='Voltshot', hash=2173046394)
+BaitAndSwitch = Item(name='Bait and Switch', hash=3078487919)
+FiringLine = Item(name='Firing Line', hash=1771339417)
+ClownCartridge = Item(name='Clown Cartridge', hash=2284787283)
+EnlightenedAction = Item(name='Enlightened Action', hash=3828510309)
+RewindRounds = Item(name='Rewind Rounds', hash=3418782618)
+Surrounded = Item(name='Surrounded', hash=3708227201)
+HighGround = Item(name='High Ground', hash=2319119708)
+FourthTimesTheCharm = Item(name="Fourth Time's the Charm", hash=1354429876)
 
 del Item
