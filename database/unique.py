9bcd6a075323e1af5444e378ee23ab22997383f8
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-06-18 02:45:58 +0300

    [db.unique] Add Exotic Class Item traits

--- database/unique.py
+++ database/unique.py
@@ -18,4 +18,63 @@ class ErgoSum:
     AggressiveFrame = Item(name='Aggressive Frame', hash=1726251414)
 
 
+class SpiritOf:
+    # region Column 1
+    # Common
+    Assassin = Item(name='Spirit of the Assassin', hash=1476923952)
+    InmostLight = Item(name='Spirit of Inmost Light', hash=1476923953)
+    Ophidian = Item(name='Spirit of the Ophidian', hash=1476923954)
+
+    # Hunter
+    Renewal = Item(name='Spirit of Renewal', hash=3751917995)
+    Caliban = Item(name='Spirit of Caliban', hash=3751917996)
+    Foetracer = Item(name='Spirit of the Foetracer', hash=3751917997)
+    Galanor = Item(name='Spirit of Galanor', hash=3751917998)
+    Dragon = Item(name='Spirit of Dragon', hash=3751917999)
+
+    # Warlock
+    Stag = Item(name='Spirit of the Stag', hash=183430248)
+    Apotheosis = Item(name='Spirit of Apotheosis', hash=183430250)
+    Necrotic = Item(name='Spirit of the Necrotic', hash=183430252)
+    Osmiomancy = Item(name='Spirit of Osmiomancy', hash=183430253)
+    Filaments = Item(name='Spirit of the Filaments', hash=183430255)
+
+    # Titan
+    Bear = Item(name='Spirit of the Bear', hash=3573490505)
+    Hoarfrost = Item(name='Spirit of Hoarfrost', hash=3573490508)
+    Severance = Item(name='Spirit of Severance', hash=3573490509)
+    Abeyant = Item(name='Spirit of the Abeyant', hash=3573490510)
+    EternalWarrior = Item(name='Spirit of the Eternal Warrior', hash=3573490511)
+
+    # endregion
+
+    # region Column 2
+    # Common
+    StarEater = Item(name='Spirit of the Star-Eater', hash=1476923955)
+    Synthoceps = Item(name='Spirit of Synthoceps', hash=1476923956)
+    Verity = Item(name='Spirit of Verity', hash=1476923957)
+
+    # Hunter
+    Coyote = Item(name='Spirit of the Coyote', hash=3751917990)
+    Wormhusk = Item(name='Spirit of the Wormhusk', hash=3751917991)
+    Liar = Item(name='Spirit of the Liar', hash=3751917992)
+    Gyrfalcon = Item(name='Spirit of the Gyrfalcon', hash=3751917993)
+    Cyrtarachne = Item(name='Spirit of the Cyrtarachne', hash=3751917994)
+
+    # Warlock
+    Swarm = Item(name='Spirit of the Swarm', hash=183430246)
+    Claw = Item(name='Spirit of the Claw', hash=183430247)
+    Starfire = Item(name='Spirit of Starfire', hash=183430249)
+    Vesper = Item(name='Spirit of Vesper', hash=183430251)
+    Harmony = Item(name='Spirit of Harmony', hash=183430254)
+
+    # Titan
+    Armamentarium = Item(name='Spirit of the Armamentarium', hash=3573490500)
+    AlphaLupi = Item(name='Spirit of Alpha Lupi', hash=3573490501)
+    Contact = Item(name='Spirit of Contact', hash=3573490504)
+    Horn = Item(name='Spirit of the Horn', hash=3573490506)
+    Scars = Item(name='Spirit of Scars', hash=3573490507)
+    # endregion
+
+
 del Item
