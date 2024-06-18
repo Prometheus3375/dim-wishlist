e0fa81c5b47d2b92257125168fe2206f014356e0
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-06-18 09:01:07 +0300

    [wishlist] Use roll()

--- wishlist.py
+++ wishlist.py
@@ -1,4 +1,4 @@
-from classes import Wishlist
+from classes import Wishlist, roll
 from database import *
 
 wishlist = Wishlist(
@@ -41,279 +41,233 @@ wishlist.add(
 
 _es_blades = {blades.EnduringBlade, blades.HungryEdge, blades.TemperedEdge}
 
-wishlist.add(
+wishlist.add_many(
     ErgoSum,
-    'Roll to activate Wolfpack Rounds on swords. '
-    'Lightweight Frame has fast heavy attack which consumes only 3 ammo',
-    {unique.ErgoSum.WolfpackRounds},
-    _es_blades,
-    {unique.ErgoSum.LightweightFrame},
+    roll(
+        'Roll to activate Wolfpack Rounds on swords. '
+        'Lightweight Frame has fast heavy attack which consumes only 3 ammo',
+        {unique.ErgoSum.WolfpackRounds},
+        _es_blades,
+        {unique.ErgoSum.LightweightFrame},
+        ),
+    roll(
+        'Good roll to get ability energy from sword kills',
+        {unique.ErgoSum.GatheringLight},
+        _es_blades,
+        {unique.ErgoSum.WaveSwordFrame},
+        ),
     )
 
-wishlist.add(
-    ErgoSum,
-    'Good roll to get ability energy from sword kills',
-    {unique.ErgoSum.GatheringLight},
-    _es_blades,
-    {unique.ErgoSum.WaveSwordFrame},
-    )
-
-# region Relativism
-wishlist.add(
+wishlist.add_many(
     Relativism,
-    'Combination Blow builds',
-    {unique.SpiritOf.Assassin, unique.SpiritOf.Caliban},
-    {unique.SpiritOf.Liar},
+    roll(
+        'Combination Blow builds',
+        {unique.SpiritOf.Assassin, unique.SpiritOf.Caliban},
+        {unique.SpiritOf.Liar},
+        ),
+    roll(
+        'Other melee builds; '
+        'Synthoceps increase damage of Combination Blow very little',
+        {unique.SpiritOf.Assassin, unique.SpiritOf.Caliban},
+        {unique.SpiritOf.Synthoceps},
+        ),
+    roll(
+        'Duskfield build',
+        {unique.SpiritOf.Renewal},
+        {unique.SpiritOf.Cyrtarachne},
+        ),
+    # roll(
+    #     'Woven Mail builds',
+    #     {unique.SpiritOf.InmostLight, unique.SpiritOf.Foetracer},
+    #     {unique.SpiritOf.Cyrtarachne},
+    #     ),
+    # roll(
+    #     'Dodge builds',
+    #     {unique.SpiritOf.Dragon},
+    #     {unique.SpiritOf.Coyote, unique.SpiritOf.Wormhusk},
+    #     ),
+    roll(
+        'Super build',
+        {unique.SpiritOf.Galanor},
+        {unique.SpiritOf.StarEater},
+        ),
+    roll(
+        'Super and weapon damage',
+        {unique.SpiritOf.Foetracer},
+        {unique.SpiritOf.StarEater},
+        ),
+    # roll(
+    #     'Gyrfalcon builds',
+    #     {unique.SpiritOf.Dragon, unique.SpiritOf.Foetracer},
+    #     {unique.SpiritOf.Gyrfalcon},
+    #     ),
+    roll(
+        'PvP build',
+        {unique.SpiritOf.Ophidian},
+        {unique.SpiritOf.Wormhusk},
+        ),
     )
 
-wishlist.add(
-    Relativism,
-    'Other melee builds; '
-    'Synthoceps increase damage of Combination Blow very little',
-    {unique.SpiritOf.Assassin, unique.SpiritOf.Caliban},
-    {unique.SpiritOf.Synthoceps},
-    )
-
-wishlist.add(
-    Relativism,
-    'Duskfield build',
-    {unique.SpiritOf.Renewal},
-    {unique.SpiritOf.Cyrtarachne},
-    )
-
-# wishlist.add(
-#     Relativism,
-#     'Woven Mail builds',
-#     {unique.SpiritOf.InmostLight, unique.SpiritOf.Foetracer},
-#     {unique.SpiritOf.Cyrtarachne},
-#     )
-
-# wishlist.add(
-#     Relativism,
-#     'Dodge builds',
-#     {unique.SpiritOf.Dragon},
-#     {unique.SpiritOf.Coyote, unique.SpiritOf.Wormhusk},
-#     )
-
-wishlist.add(
-    Relativism,
-    'Super build',
-    {unique.SpiritOf.Galanor},
-    {unique.SpiritOf.StarEater},
-    )
-
-wishlist.add(
-    Relativism,
-    'Super and weapon damage',
-    {unique.SpiritOf.Foetracer},
-    {unique.SpiritOf.StarEater},
-    )
-
-# wishlist.add(
-#     Relativism,
-#     'Gyrfalcon builds',
-#     {unique.SpiritOf.Dragon, unique.SpiritOf.Foetracer},
-#     {unique.SpiritOf.Gyrfalcon},
-#     )
-
-wishlist.add(
-    Relativism,
-    'PvP build',
-    {unique.SpiritOf.Ophidian},
-    {unique.SpiritOf.Wormhusk},
-    )
-# endregion
-
-
-# region Stoicism
-wishlist.add(
+wishlist.add_many(
     Stoicism,
-    'Melee builds',
-    {unique.SpiritOf.Assassin, unique.SpiritOf.InmostLight, unique.SpiritOf.Severance},
-    {unique.SpiritOf.Synthoceps, unique.SpiritOf.Contact},
+    roll(
+        'Melee builds',
+        {unique.SpiritOf.Assassin, unique.SpiritOf.InmostLight, unique.SpiritOf.Severance},
+        {unique.SpiritOf.Synthoceps, unique.SpiritOf.Contact},
+        ),
+    roll(
+        'Barricade builds',
+        {unique.SpiritOf.Hoarfrost},
+        {unique.SpiritOf.AlphaLupi, unique.SpiritOf.Horn},
+        ),
+    roll(
+        # todo Check if Verity buffs damage from Unbreakable
+        'Unbreakable build',
+        {unique.SpiritOf.Bear},
+        {unique.SpiritOf.Armamentarium},
+        ),
+    roll(
+        "Drengr's Lash build",
+        {unique.SpiritOf.Abeyant},
+        {unique.SpiritOf.Horn},
+        ),
+    roll(
+        'Super and weapon damage',
+        {unique.SpiritOf.EternalWarrior},
+        {unique.SpiritOf.StarEater},
+        ),
+    roll(
+        'PvP build',
+        {unique.SpiritOf.Ophidian},
+        {unique.SpiritOf.AlphaLupi},
+        ),
     )
 
-wishlist.add(
-    Stoicism,
-    'Barricade builds',
-    {unique.SpiritOf.Hoarfrost},
-    {unique.SpiritOf.AlphaLupi, unique.SpiritOf.Horn},
-    )
-
-# todo Check if Verity buffs damage from Unbreakable
-wishlist.add(
-    Stoicism,
-    'Unbreakable build',
-    {unique.SpiritOf.Bear},
-    {unique.SpiritOf.Armamentarium},
-    )
-
-wishlist.add(
-    Stoicism,
-    "Drengr's Lash build",
-    {unique.SpiritOf.Abeyant},
-    {unique.SpiritOf.Horn},
-    )
-
-wishlist.add(
-    Stoicism,
-    'Super and weapon damage',
-    {unique.SpiritOf.EternalWarrior},
-    {unique.SpiritOf.StarEater},
-    )
-
-wishlist.add(
-    Stoicism,
-    'PvP build',
-    {unique.SpiritOf.Ophidian},
-    {unique.SpiritOf.AlphaLupi},
-    )
-# endregion
-
-
-# region Solipsism
-wishlist.add(
+wishlist.add_many(
     Solipsism,
-    'Melee builds',
-    {unique.SpiritOf.Assassin, unique.SpiritOf.InmostLight, unique.SpiritOf.Necrotic},
-    {unique.SpiritOf.Synthoceps, unique.SpiritOf.Claw},
+    roll(
+        'Melee builds',
+        {unique.SpiritOf.Assassin, unique.SpiritOf.InmostLight, unique.SpiritOf.Necrotic},
+        {unique.SpiritOf.Synthoceps, unique.SpiritOf.Claw},
+        ),
+    # roll(
+    #     'Grenade build',
+    #     {unique.SpiritOf.Osmiomancy},
+    #     {unique.SpiritOf.Verity},
+    #     ),
+    roll(
+        'Empowered Rift combo',
+        {unique.SpiritOf.Filaments},
+        {unique.SpiritOf.Starfire},
+        ),
+    # roll(
+    #     'Other Rift builds',
+    #     {unique.SpiritOf.InmostLight, unique.SpiritOf.Stag},
+    #     {unique.SpiritOf.Vesper},
+    #     ),
+    roll(
+        'Super builds',
+        {unique.SpiritOf.Apotheosis},
+        {unique.SpiritOf.Harmony, unique.SpiritOf.StarEater},
+        ),
+    roll(
+        'Song of Flame grenade build',
+        {unique.SpiritOf.Apotheosis},
+        {unique.SpiritOf.Verity},
+        ),
+    roll(
+        'PvP build',
+        {unique.SpiritOf.Ophidian},
+        {unique.SpiritOf.Harmony},
+        ),
     )
-
-# wishlist.add(
-#     Solipsism,
-#     'Grenade build',
-#     {unique.SpiritOf.Osmiomancy},
-#     {unique.SpiritOf.Verity},
-#     )
-
-wishlist.add(
-    Solipsism,
-    'Empowered Rift combo',
-    {unique.SpiritOf.Filaments},
-    {unique.SpiritOf.Starfire},
-    )
-
-# wishlist.add(
-#     Solipsism,
-#     'Other Rift builds',
-#     {unique.SpiritOf.InmostLight, unique.SpiritOf.Stag},
-#     {unique.SpiritOf.Vesper},
-#     )
-
-wishlist.add(
-    Solipsism,
-    'Super builds',
-    {unique.SpiritOf.Apotheosis},
-    {unique.SpiritOf.Harmony, unique.SpiritOf.StarEater},
-    )
-
-wishlist.add(
-    Solipsism,
-    'Song of Flame grenade build',
-    {unique.SpiritOf.Apotheosis},
-    {unique.SpiritOf.Verity},
-    )
-
-wishlist.add(
-    Solipsism,
-    'PvP build',
-    {unique.SpiritOf.Ophidian},
-    {unique.SpiritOf.Harmony},
-    )
-# endregion
 
 # endregion
 
 # region Echoes
 
-wishlist.add(
+wishlist.add_many(
     Breachlight,
-    'Roll for grenade builds',
-    {traits.Demolitionist},
-    {traits.AdrenalineJunkie},
+    roll(
+        'Roll for grenade builds',
+        {traits.Demolitionist},
+        {traits.AdrenalineJunkie},
+        ),
+    roll(
+        'Roll for melee builds and synergy with the origin trait',
+        {traits.Pugilist},
+        {traits.Swashbuckler},
+        ),
+    roll(
+        'Roll for Strand builds',
+        {traits.Slice},
+        {traits.Hatchling},
+        ),
+    roll(
+        'Good PvE rolls',
+        {traits.ThreatDetector, traits.Demolitionist},
+        {traits.DesperateMeasures, traits.Hatchling},
+        ),
     )
 
-wishlist.add(
-    Breachlight,
-    'Roll for melee builds and synergy with the origin trait',
-    {traits.Pugilist},
-    {traits.Swashbuckler},
-    )
-
-wishlist.add(
-    Breachlight,
-    'Roll for Strand builds',
-    {traits.Slice},
-    {traits.Hatchling},
-    )
-
-wishlist.add(
-    Breachlight,
-    'Good PvE rolls',
-    {traits.ThreatDetector, traits.Demolitionist},
-    {traits.DesperateMeasures, traits.Hatchling},
-    )
-
-wishlist.add(
+wishlist.add_many(
     PatronOfLostCauses,
-    'Rolls to get class ability energy from afar',
-    {traits.Strategist},
-    {traits.ExplosivePayload, traits.KineticTremors},
-    )
-
-wishlist.add(
-    PatronOfLostCauses,
-    'Classic PvE rolls for a scout rifle',
-    {traits.RapidHit},
-    {traits.ExplosivePayload, traits.KineticTremors},
+    roll(
+        'Rolls to get class ability energy from afar',
+        {traits.Strategist},
+        {traits.ExplosivePayload, traits.KineticTremors},
+        ),
+    roll(
+        'Classic PvE rolls for a scout rifle',
+        {traits.RapidHit},
+        {traits.ExplosivePayload, traits.KineticTremors},
+        ),
     )
 
 _perfect_paradox_barrels = {barrels.Smallbore, barrels.CorkscrewRifling, barrels.BarrelShroud}
 _perfect_paradox_mags = {magazines.TacticalMag}
 
-wishlist.add(
+wishlist.add_many(
     PerfectParadox,
-    'Special rolls for melee damage increase',
-    _perfect_paradox_barrels,
-    _perfect_paradox_mags,
-    {traits.FieldPrep},
-    {traits.OneTwoPunch},
+    roll(
+        'Rolls for melee damage increase',
+        _perfect_paradox_barrels,
+        _perfect_paradox_mags,
+        {traits.FieldPrep},
+        {traits.OneTwoPunch},
+        ),
+    roll(
+        'Rolls for shotgun DPS',
+        _perfect_paradox_barrels,
+        _perfect_paradox_mags,
+        {traits.ThreatDetector, traits.FieldPrep},
+        {traits.TrenchBarrel},
+        )
     )
 
-wishlist.add(
-    PerfectParadox,
-    'Rolls for shotgun DPS',
-    _perfect_paradox_barrels,
-    _perfect_paradox_mags,
-    {traits.ThreatDetector, traits.FieldPrep},
-    {traits.TrenchBarrel},
-    )
-
-wishlist.add(
+wishlist.add_many(
     MartyrsRetribution,
-    'Special Clip combo',
-    {barrels.VolatileLaunch},
-    {magazines.HighVelocityRounds},
-    {traits.HealClip},
-    {traits.KillClip},
-    )
-
-wishlist.add(
-    MartyrsRetribution,
-    'Roll for grenade builds',
-    {barrels.VolatileLaunch},
-    {magazines.HighVelocityRounds},
-    {traits.Demolitionist},
-    {traits.AdrenalineJunkie},
-    )
-
-wishlist.add(
-    MartyrsRetribution,
-    'Good PvE rolls',
-    {barrels.VolatileLaunch},
-    {magazines.HighVelocityRounds},
-    {traits.Demolitionist, traits.HealClip, traits.AutoLoadingHolster},
-    {traits.DesperateMeasures, traits.Incandescent},
+    roll(
+        'Clip combo',
+        {barrels.VolatileLaunch},
+        {magazines.HighVelocityRounds},
+        {traits.HealClip},
+        {traits.KillClip},
+        ),
+    roll(
+        'Roll for grenade builds',
+        {barrels.VolatileLaunch},
+        {magazines.HighVelocityRounds},
+        {traits.Demolitionist},
+        {traits.AdrenalineJunkie},
+        ),
+    roll(
+        'Good PvE rolls',
+        {barrels.VolatileLaunch},
+        {magazines.HighVelocityRounds},
+        {traits.Demolitionist, traits.HealClip, traits.AutoLoadingHolster},
+        {traits.DesperateMeasures, traits.Incandescent},
+        ),
     )
 
 wishlist.add(
