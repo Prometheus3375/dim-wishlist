dc8569084a45b781d2c1154d2dce3c82ff0c31ca
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-06-23 02:59:15 +0300

    [db] Add more magazines and barrels

--- database/barrels.py
+++ database/barrels.py
@@ -1,9 +1,15 @@
 from classes import Item
 
-Smallbore = Item(name='Smallbore', hash=1482024992)
-BarrelShroud = Item(name='Barrel Shroud', hash=1300023272)
-CorkscrewRifling = Item(name='Corkscrew Rifling', hash=4090651448)
 ArrowheadBrake = Item(name='Arrowhead Brake', hash=839105230)
+BarrelShroud = Item(name='Barrel Shroud', hash=1300023272)
+ChamberedCompensator = Item(name='Chambered Compensator', hash=3661387068)
+CorkscrewRifling = Item(name='Corkscrew Rifling', hash=4090651448)
+ExtendedBarrel = Item(name='Extended Barrel', hash=1467527085)
+FlutedBarrel = Item(name='Fluted Barrel', hash=1840239774)
+FullBore = Item(name='Full Bore', hash=202670084)
+HammerForgedRifling = Item(name='Hammer-Forged Rifling', hash=3250034553)
+PolygonalRifling = Item(name='Polygonal Rifling', hash=1392496348)
+Smallbore = Item(name='Smallbore', hash=1482024992)
 
 VolatileLaunch = Item(name='Volatile Launch', hash=1478423395)
 QuickLaunch = Item(name='Quick Launch', hash=3525010810)
--- database/magazines.py
+++ database/magazines.py
@@ -1,6 +1,13 @@
 from classes import Item
 
+AccurizedRounds = Item(name='Accurized Rounds', hash=3142289711)
+AppendedMag = Item(name='Appended Mag', hash=1087426260)
 TacticalMag = Item(name='Tactical Mag', hash=106909392)
+ExtendedMag = Item(name='Extended Mag', hash=2420895100)
+SteadyRounds = Item(name='Steady Rounds', hash=3177308360)
+AlloyMagazine = Item(name='Alloy Magazine', hash=1431678320)
+FlaredMagwell = Item(name='Flared Magwell', hash=3230963543)
+
 HighVelocityRounds = Item(name='High-Velocity Rounds', hash=2822142346)
 
 del Item
