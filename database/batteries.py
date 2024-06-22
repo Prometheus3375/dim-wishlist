947a2181c8dbabf24e8b37eb10d2386f0151f0c0
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-06-23 02:49:07 +0300

    [db] Add batteries

---
+++ database/batteries.py
@@ -0,0 +1,10 @@
+from classes import Item
+
+AcceleratedCoils = Item(name='Accelerated Coils', hash=689005463)
+EnhancedBattery = Item(name='Enhanced Battery', hash=2680121939)
+IonizedBattery = Item(name='Ionized Battery', hash=1483536627)
+LiquidCoils = Item(name='Liquid Coils', hash=1687452232)
+ParticleRepeater = Item(name='Particle Repeater', hash=3868766766)
+ProjectionFuse = Item(name='Projection Fuse', hash=2969185026)
+
+del Item
