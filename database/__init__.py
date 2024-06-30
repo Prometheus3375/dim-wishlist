155d838f1c571d7df64ee6da926fa8b8f6ff6f90
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-06-30 20:04:00 +0300

    [db] Add Veist Stringer

--- database/__init__.py
+++ database/__init__.py
@@ -1,3 +1,3 @@
-from . import barrels, batteries, blades, guards, magazines, masterworks, traits, unique
+from . import barrels, batteries, blades, guards, magazines, masterworks, origin, traits, unique
 from .categories import *
 from .items import *
