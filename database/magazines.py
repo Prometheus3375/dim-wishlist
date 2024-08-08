60ae80519da8a46e6d65838aa8caf440c170ab00
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-08-08 03:48:45 +0300

    [db] Add Light Mag

--- database/magazines.py
+++ database/magazines.py
@@ -7,6 +7,7 @@ ExtendedMag = Item(name='Extended Mag', hash=2420895100)
 SteadyRounds = Item(name='Steady Rounds', hash=3177308360)
 AlloyMagazine = Item(name='Alloy Magazine', hash=1431678320)
 FlaredMagwell = Item(name='Flared Magwell', hash=3230963543)
+LightMag = Item(name='Light Mag', hash=679225683)
 
 HighVelocityRounds = Item(name='High-Velocity Rounds', hash=2822142346)
 
