1b09cd188cd254c3232355503afc2d9d1b84ace2
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-10-17 02:19:47 +0300

    [database] Add magazines

--- database/magazines.py
+++ database/magazines.py
@@ -1,15 +1,31 @@
 from classes import Item
 
 AccurizedRounds = Item(name='Accurized Rounds', hash=3142289711)
-AppendedMag = Item(name='Appended Mag', hash=1087426260)
-TacticalMag = Item(name='Tactical Mag', hash=106909392)
-ExtendedMag = Item(name='Extended Mag', hash=2420895100)
+ArmorPiercingRounds = Item(name='Armor-Piercing Rounds', hash=1968497646)
+HighCaliberRounds = Item(name='High-Caliber Rounds', hash=1561002382)
+RicochetRounds = Item(name='Ricochet Rounds', hash=1885400500)
 SteadyRounds = Item(name='Steady Rounds', hash=3177308360)
+TacticalMag = Item(name='Tactical Mag', hash=106909392)
+AppendedMag = Item(name='Appended Mag', hash=1087426260)
+ExtendedMag = Item(name='Extended Mag', hash=2420895100)
 AlloyMagazine = Item(name='Alloy Magazine', hash=1431678320)
 FlaredMagwell = Item(name='Flared Magwell', hash=3230963543)
 LightMag = Item(name='Light Mag', hash=679225683)
 AssaultMag = Item(name='Assault Mag', hash=791862061)
+SeraphRounds = Item(name='Seraph Rounds', hash=1140096971)
 
+AlloyCasing = Item(name='Alloy Casing', hash=2985827016)
+BlackPowder = Item(name='Black Powder', hash=1996142143)
 HighVelocityRounds = Item(name='High-Velocity Rounds', hash=2822142346)
+ImplosionRounds = Item(name='Implosion Rounds', hash=3492396210)
+ImpactCasing = Item(name='Impact Casing', hash=3796465595)
+
+ProximityGrenades = Item(name='Proximity Grenades', hash=409831596)
+SpikeGrenades = Item(name='Spike Grenades', hash=3301904089)
+AugmentedDrum = Item(name='Augmented Drum', hash=1771897777)
+HighExplosiveOrdnance = Item(name='High-Explosive Ordnance', hash=1380253176)
+MiniFrags = Item(name='Mini Frags', hash=332133599)
+StickyGrenades = Item(name='Sticky Grenades', hash=3373736292)
+DisorientingGrenades = Item(name='Disorienting Grenades', hash=3032599245)
 
 del Item
