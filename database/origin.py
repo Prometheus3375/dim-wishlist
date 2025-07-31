fc199a04545d1136e01b1db787815508ff4e5808
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2025-07-31 04:51:56 +0300

    [db] Add traits

--- database/origin.py
+++ database/origin.py
@@ -3,7 +3,19 @@ from classes import Item
 VeistStinger = Item(name='Veist Stinger', hash=3988215619)
 OmolonFluidDynamics = Item(name='Omolon Fluid Dynamics', hash=2839173408)
 WildCard = Item(name='Wild Card', hash=1050127423)
+HakkeBreachArmaments = Item(name='Hakke Breach Armaments', hash=1607056502)
+SurosSynergy = Item(name='Suros Synergy', hash=4008116374)
+
 CrossingOver = Item(name='Crossing Over', hash=4129855581)
 GravityWell = Item(name='Gravity Well', hash=59871045)
+FleetFooted = Item(name='Fleet Footed', hash=65797256)
+RoarOfBattle = Item(name='Roar Of Battle', hash=1673863459)
+ExhaustiveResearch = Item(name='Exhaustive Research', hash=1906137414)
+FrameOfReference = Item(name='FrameOfReference', hash=2597494435)
+VanguardDetermination = Item(name='Vanguard Determination', hash=3846229553)
+Tenacity = Item(name='Tenacity', hash=3911709647)
+ProblemSolver = Item(name='Problem Solver', hash=3454102905)
+DreamWork = Item(name='Dream Work', hash=1380061575)
+BrayLegacy = Item(name='Bray Legacy', hash=4139916815)
 
 del Item
