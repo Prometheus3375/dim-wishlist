44a52f130671bc430d941c4ca78293e548ed3ad2
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2025-07-31 04:51:41 +0300

    [db] Add grips, rails, bolts

---
+++ database/bolts.py
@@ -0,0 +1,9 @@
+from classes import Item
+
+ExplosiveBolts = Item(name='Explosive Bolts', hash=3603807747)
+SpringAugerBolts = Item(name='Spring-Auger Bolts', hash=2136073194)
+ChargedBolts = Item(name='Charged Bolts', hash=2955427634)
+SerratedBolts = Item(name='Serrated Bolts', hash=1069117426)
+HeavyBolts = Item(name='Heavy Bolts', hash=1202133695)
+
+del Item
