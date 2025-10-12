f582eb44035e054d5996590c947f2db8c0109f85
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2025-10-12 15:38:16 +0300

    [wishlist] Create wishlist in a function

--- wishlist/__main__.py
+++ wishlist/__main__.py
@@ -3,22 +3,29 @@ from importlib import import_module
 
 from classes import RollDefinition, Wishlist
 
-this_dir = os.path.dirname(__file__)
-this_package = os.path.basename(this_dir)
-for dirpath, dirnames, filenames in os.walk(this_dir):
-    module_path = os.path.relpath(dirpath, this_dir).replace(os.sep, '.')
-    for file in filenames:
-        if file.endswith('.py') and not file.startswith('_'):
-            import_module(f'{this_package}.{module_path}.{file[:-3]}')
+__all__ = 'define_wishlist',
 
-wishlist = Wishlist(
-    title='Wishlist made by Prometheus3375',
-    description='Rolls on weapons and armor Prometheus3375 would like to have',
-    )
 
-for cls in RollDefinition.__subclasses__():
-    for item in cls.items:
-        wishlist.add_many(item, *cls.rolls)
+def define_wishlist() -> Wishlist:
+    this_dir = os.path.dirname(__file__)
+    this_package = os.path.basename(this_dir)
+    for dirpath, dirnames, filenames in os.walk(this_dir):
+        module_path = os.path.relpath(dirpath, this_dir).replace(os.sep, '.')
+        for file in filenames:
+            if file.endswith('.py') and not file.startswith('_'):
+                import_module(f'{this_package}.{module_path}.{file[:-3]}')
+
+    wishlist = Wishlist(
+        title='Wishlist made by Prometheus3375',
+        description='Rolls on weapons and armor Prometheus3375 would like to have',
+        )
+
+    for cls in RollDefinition.__subclasses__():
+        for item in cls.items:
+            wishlist.add_many(item, *cls.rolls)
+
+    return wishlist
+
 
 if __name__ == '__main__':
-    wishlist.to_dim_wishlist_file('wishlist.txt')
+    define_wishlist().to_dim_wishlist_file('wishlist.txt')
