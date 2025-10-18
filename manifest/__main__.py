095a3adff3c0fb4914cad9292b4fdb2269222f5a
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2025-10-19 02:43:37 +0300

    [manifest.main] Fix cmd parser

--- manifest/__main__.py
+++ manifest/__main__.py
@@ -170,7 +170,7 @@ def parse_cmd_arguments() -> Namespace:
         add_help=False,
         )
 
-    parser_list.set_defaults(function=list_commands)
+    parser_generate.set_defaults(function=generate_commands)
     parser_generate.add_argument(*help_args, **help_kwargs)
 
     perk_database_default = 'releases.v800.annual'
