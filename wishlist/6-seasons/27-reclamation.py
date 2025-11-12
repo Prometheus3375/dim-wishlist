d4549cf1fc7e899c6f24047f76c13817c2ce4d8e
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2025-11-12 20:29:31 +0300

    [wishlist] Update Romantic Death and Submersion

--- wishlist/6-seasons/27-reclamation.py
+++ wishlist/6-seasons/27-reclamation.py
@@ -1,7 +1,102 @@
 from wishlist import *
 
 
+class PhoneutriaFera(RollDefinition):
+    """
+    Solar hand cannon, Spread Shot
+    https://www.light.gg/db/items/3496887154
+    """
+    items = [
+        Item('Phoneutria Fera', hash=3496887154),
+        Item('Phoneutria Fera', hash=3804242792),
+        Item('Phoneutria Fera', hash=3804242793),
+        Item('Phoneutria Fera', hash=3804242794),
+        Item('Phoneutria Fera', hash=3804242795),
+        ]
+    rolls = [
+        Roll(
+            'Melee damage increase',
+            [barrel.FlutedBarrel, AnyPerk],
+            [magazine.TacticalMag, AnyPerk],
+            [trait.ThreatDetector],
+            [trait.OneTwoPunch],
+            ),
+        Roll(
+            'Damage dealing',
+            [barrel.FlutedBarrel, AnyPerk],
+            [magazine.AppendedMag, AnyPerk],
+            [trait.ThreatDetector],
+            [trait.Surrounded],
+            ),
+        Roll(
+            'Ad clear',
+            [barrel.FlutedBarrel, AnyPerk],
+            [magazine.TacticalMag, AnyPerk],
+            [trait.ThreatDetector],
+            [trait.Incandescent],
+            ),
+        ]
+
+
 # Special
+
+
+class Ribbontail(RollDefinition):
+    """
+    Strand Trace Rifle, Adaptive Frame
+    https://www.light.gg/db/items/3576134513
+    """
+    items = [
+        Item('Ribbontail', hash=3576134513),
+        Item('Ribbontail', hash=407150808),
+        Item('Ribbontail', hash=407150809),
+        Item('Ribbontail', hash=407150810),
+        Item('Ribbontail', hash=407150811),
+        ]
+    roll = Roll(
+        'Ad clear',
+        [barrel.Smallbore, AnyPerk],
+        [battery.LightBattery, AnyPerk],
+        [trait.Subsistence],
+        [trait.Redirection, trait.DetonatorBeam],
+        )
+
+
+class Trachinus(RollDefinition):
+    """
+    Stasis Shotgun, Rapid Fire Slug
+    https://www.light.gg/db/items/3635232671
+    """
+    items = [
+        Item('Trachinus', hash=3635232671),
+        Item('Trachinus', hash=2888021252),
+        Item('Trachinus', hash=2888021253),
+        Item('Trachinus', hash=2888021254),
+        Item('Trachinus', hash=2888021255),
+        ]
+    rolls = [
+        Roll(
+            'Stasis combo',
+            [barrel.ArrowheadBrake, AnyPerk],
+            [magazine.AssaultMag, AnyPerk],
+            [trait.Rimestealer],
+            [trait.Headstone],
+            ),
+        Roll(
+            'Chill Clip',
+            [barrel.ArrowheadBrake, AnyPerk],
+            [magazine.AssaultMag, AnyPerk],
+            [trait.RapidHit, trait.LeadFromGold],
+            [trait.ChillClip],
+            ),
+        Roll(
+            'Damage dealing',
+            [barrel.ArrowheadBrake, AnyPerk],
+            [magazine.AssaultMag, AnyPerk],
+            [trait.RapidHit, trait.LeadFromGold],
+            [trait.PrecisionInstrument],
+            ),
+        ]
 
 
 class RomanticDeath(RollDefinition):
@@ -21,7 +116,7 @@ class RomanticDeath(RollDefinition):
             'Ad clear',
             [launcher_barrel.VolatileLaunch, AnyPerk],
             [magazine.HighVelocityRounds],
-            [trait.ThreatDetector, trait.FeedingFrenzy],
+            [trait.FeedingFrenzy],
             [trait.DestabilizingRounds, trait.OneForAll, trait.ChainReaction],
             ),
         Roll(
@@ -35,6 +130,43 @@ class RomanticDeath(RollDefinition):
 
 
 # Heavy
+
+
+class Synanceia(RollDefinition):
+    """
+    Solar Sword, Wave Sword Frame
+    https://www.light.gg/db/items/1823308496
+    """
+    items = [
+        Item('Synanceia', hash=1823308496),
+        Item('Synanceia', hash=2765451288),
+        Item('Synanceia', hash=2765451289),
+        Item('Synanceia', hash=2765451290),
+        Item('Synanceia', hash=2765451291),
+        ]
+    rolls = [
+        Roll(
+            'Movement',
+            [blade.HonedEdge, AnyPerk],
+            [guard.SwordmastersGuard, AnyPerk],
+            [trait.AssassinsBlade],
+            [trait.EagerEdge],
+            ),
+        Roll(
+            'Ad clear',
+            [blade.JaggedEdge, AnyPerk],
+            [guard.SwordmastersGuard, AnyPerk],
+            [trait.RelentlessStrikes],
+            [trait.ChainReaction],
+            ),
+        Roll(
+            'Damage dealing',
+            [blade.JaggedEdge, AnyPerk],
+            [guard.SwordmastersGuard, AnyPerk],
+            [trait.RelentlessStrikes],
+            [trait.Surrounded, trait.ElementalHoning],
+            ),
+        ]
 
 
 class FoldedRoot(RollDefinition):
@@ -72,21 +204,21 @@ class Submersion(RollDefinition):
             'Ad clear',
             [rail.LowProfileRail, AnyPerk],
             [bolt.ExplosiveBolts, AnyPerk],
-            [trait.AutoLoadingHolster, trait.ImpulseAmplifier, trait.BoltScavenger],
+            [trait.ImpulseAmplifier, trait.BoltScavenger, trait.AutoLoadingHolster],
             [trait.Headstone],
             ),
         Roll(
             'Chill Clip',
             [rail.LowProfileRail, AnyPerk],
             [bolt.SerratedBolts, AnyPerk],
-            [trait.AutoLoadingHolster, trait.ImpulseAmplifier, trait.BoltScavenger],
+            [trait.ImpulseAmplifier, trait.BoltScavenger, trait.AutoLoadingHolster],
             [trait.ChillClip],
             ),
         Roll(
             'Damage dealing',
             [rail.LowProfileRail, AnyPerk],
             [bolt.HeavyBolts, AnyPerk],
-            [trait.AutoLoadingHolster, trait.ImpulseAmplifier, trait.BoltScavenger],
+            [trait.ImpulseAmplifier, trait.BoltScavenger, trait.AutoLoadingHolster],
             [trait.AggregateCharge, trait.HighGround, trait.FiringLine],
             ),
         ]
