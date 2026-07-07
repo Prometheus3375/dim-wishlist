158a48243ed2e20604cbc4f2aa60a4af5139b97b
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2026-07-07 15:36:26 +0300

    [wishlist] Update solar bows

--- wishlist/1-primary/bows/3-solar-bows.py
+++ wishlist/1-primary/bows/3-solar-bows.py
@@ -1,5 +1,10 @@
 from wishlist import *
 
+_precision_strings = [bowstring.ElasticString, AnyPerk]
+_precision_arrows = [arrow.CompactArrowShaft, AnyPerk]
+_lightweight_strings = [bowstring.PolymerString, AnyPerk]
+_lightweight_arrows = [arrow.FiberglassArrowShaft, AnyPerk]
+
 
 class Hush(RollDefinition):
     """
@@ -9,6 +14,34 @@ class Hush(RollDefinition):
     https://destiny.report/w/3638723317
     """
     item = Item('Hush', hash=3638723317)
+    is_chosen = True
+    rolls = [
+        Roll(
+            'Super roll',
+            _precision_strings,
+            _precision_arrows,
+            [trait.ArchersTempo],
+            [trait.HipFireGrip],
+            [trait.Firefly],
+            [trait.Incandescent],
+            [trait.ArchersGambit],
+            [trait.Meganeura],
+            ),
+        Roll(
+            'Ad clear',
+            _precision_strings,
+            _precision_arrows,
+            [trait.ArchersTempo, trait.Firefly],
+            [trait.Incandescent, trait.Meganeura],
+            ),
+        Roll(
+            'Hip-fire combo',
+            _precision_strings,
+            [arrow.FiberglassArrowShaft, AnyPerk],
+            [trait.HipFireGrip],
+            [trait.ArchersGambit],
+            ),
+        ]
 
 
 class PreAstyanaxIV(RollDefinition):
@@ -19,13 +52,24 @@ class PreAstyanaxIV(RollDefinition):
     https://destiny.report/w/471764396
     """
     item = Item('Pre Astyanax IV', hash=471764396)
-    roll = Roll(
-        'Solar combo',
-        [bowstring.ElasticString, AnyPerk],
-        [arrow.CompactArrowShaft, AnyPerk],
-        [trait.Incandescent],
-        [trait.BurningAmbition],
-        )
+    roll = [
+        Roll(
+            'Super roll',
+            _precision_strings,
+            _precision_arrows,
+            [trait.Firefly],
+            [trait.Incandescent],
+            [trait.BurningAmbition],
+            [trait.ExplosiveHead],
+            ),
+        Roll(
+            'Solar combo',
+            _precision_strings,
+            _precision_arrows,
+            [trait.Incandescent],
+            [trait.BurningAmbition],
+            ),
+        ]
 
 
 class TyrannyOfHeaven(RollDefinition):
@@ -36,3 +80,10 @@ class TyrannyOfHeaven(RollDefinition):
     https://destiny.report/w/3388655311
     """
     item = Item('Tyranny of Heaven', hash=3388655311)
+    roll = Roll(
+        'Solar combo',
+        _lightweight_strings,
+        _lightweight_arrows,
+        [trait.BurningAmbition],
+        [trait.Incandescent],
+        ),
