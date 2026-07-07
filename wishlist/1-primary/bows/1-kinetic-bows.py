6356923e7979ed8eaf9c6c51e5d9a9ccb5867bf2
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2026-07-07 15:00:22 +0300

    [wishlist] Update kinetic bows

--- wishlist/1-primary/bows/1-kinetic-bows.py
+++ wishlist/1-primary/bows/1-kinetic-bows.py
@@ -1,5 +1,10 @@
 from wishlist import *
 
+_precision_strings = [bowstring.ElasticString, AnyPerk]
+_precision_arrows = [arrow.CompactArrowShaft, AnyPerk]
+_lightweight_strings = [bowstring.PolymerString, AnyPerk]
+_lightweight_arrows = [arrow.FiberglassArrowShaft, AnyPerk]
+
 
 class AccruedRedemption(RollDefinition):
     """
@@ -9,6 +14,13 @@ class AccruedRedemption(RollDefinition):
     https://destiny.report/w/3621336854
     """
     item = Item('Accrued Redemption', hash=3621336854)
+    roll = Roll(
+        'Ad clear',
+        _precision_strings,
+        _precision_arrows,
+        [trait.ArchersTempo, trait.AttritionOrbs],
+        [trait.KineticTremors],
+        )
 
 
 class BitingWinds(RollDefinition):
@@ -19,6 +31,19 @@ class BitingWinds(RollDefinition):
     https://destiny.report/w/2485400469
     """
     item = Item('Biting Winds', hash=2485400469)
+    is_chosen = True
+    rolls = [
+        Roll(
+            'Super roll',
+            _precision_strings,
+            _precision_arrows,
+            [trait.ArchersTempo],
+            [trait.BewilderingBurst],
+            [trait.ImpromptuAmmunition],
+            [trait.ExplosiveHead],
+            [trait.KineticTremors],
+            ),
+        ]
 
 
 class FelTaradiddle(RollDefinition):
@@ -29,20 +54,46 @@ class FelTaradiddle(RollDefinition):
     https://destiny.report/w/1399109800
     """
     item = Item('Fel Taradiddle', hash=1399109800)
+    roll = Roll(
+        'Ad clear',
+        _lightweight_strings,
+        _lightweight_arrows,
+        [trait.ArchersTempo],
+        [trait.OneForAll],
+        )
 
 
 class MercuryA(RollDefinition):
     """
-    Kinetic Combat Bow, High-Impact Longbow, Anti-Unstoppable
+    Kinetic High-Impact Longbow, Anti-Unstoppable
     Source: Commander Zavala
     https://www.light.gg/db/items/2838279629
     https://destiny.report/w/2838279629
     """
     item = Item('Mercury-A', hash=2838279629)
-    roll = Roll(
-        "Hit combo; hits with Kinetic Tremors grant progress for Attrition Orbs",
-        [bowstring.ElasticString, AnyPerk],
-        [arrow.CompactArrowShaft, AnyPerk],
-        [trait.AttritionOrbs],
-        [trait.KineticTremors],
-        )
+    is_chosen = True
+    rolls = [
+        Roll(
+            'Super roll',
+            _precision_strings,
+            _precision_arrows,
+            [trait.Demolitionist],
+            [trait.AttritionOrbs],
+            [trait.KineticTremors],
+            [trait.AdrenalineJunkie],
+            ),
+        Roll(
+            "Hit combo; hits with Kinetic Tremors grant progress for Attrition Orbs",
+            _precision_strings,
+            _precision_arrows,
+            [trait.AttritionOrbs],
+            [trait.KineticTremors],
+            ),
+        Roll(
+            'Grenade combo',
+            _precision_strings,
+            _precision_arrows,
+            [trait.Demolitionist],
+            [trait.AdrenalineJunkie],
+            ),
+        ]
