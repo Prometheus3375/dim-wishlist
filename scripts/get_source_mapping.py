b60721de7904648b8718043457b371f029561557
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2026-07-12 22:48:38 +0300

    [scripts] Update source mapping script

--- scripts/get_source_mapping.py
+++ scripts/get_source_mapping.py
@@ -4,7 +4,7 @@ from wishlist import RollDefinition
 from wishlist.__main__ import define_wishlist
 
 _ = define_wishlist()
-sources = set()
+found_sources = set()
 hash2weapon = {}
 for cls in RollDefinition.__subclasses__():
     docs = cls.__doc__.strip().splitlines()
@@ -12,14 +12,15 @@ for cls in RollDefinition.__subclasses__():
         print(f'Class {cls.__module__}.{cls.__qualname__} has old description.')
         continue
 
-    source_line = docs[1].strip()[7:].strip()
-    sources.add(source_line)
+    source_line = docs[1].strip()[7:]
+    sources = [s.strip() for s in source_line.split(';')]
+    found_sources.update(sources)
     for item in cls.items:
-        hash2weapon[str(item.hash)] = dict(name=item.name, source=source_line)
+        hash2weapon[item.hash] = dict(name=item.name, sources=sources)
 
 with open('source-mapping.json', 'w') as f:
     json.dump(
-        dict(sources=sorted(sources), weapons=hash2weapon),
+        dict(sources=sorted(found_sources), weapons=hash2weapon),
         f,
         ensure_ascii=False,
         indent=2,
