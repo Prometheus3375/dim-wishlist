435494d31a74064d090fd65a08545d96133ae18d
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2026-07-20 00:40:39 +0300

    [wishlist] Update kinetic shotguns

--- wishlist/2-special/shotguns/__init__.py
+++ wishlist/2-special/shotguns/__init__.py
@@ -5,5 +5,5 @@ from wishlist import AnyPerk, barrel, magazine
 pvp_barrels = [barrel.BarrelShroud, barrel.CorkscrewRifling, barrel.Smallbore]
 pvp_mags = [magazine.AccurizedRounds, magazine.LightMag]
 
-pve_barrels = [barrel.Smallbore, AnyPerk]
+pve_barrels = [barrel.BarrelShroud, AnyPerk]
 pve_mags = [magazine.TacticalMag, AnyPerk]
