0871328d67cf8f7bff56801915e0927b80786ac6
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2026-07-26 02:23:43 +0300

    [wishlist] Update Uncivil Discourse

--- wishlist/2-special/hand-cannons.py
+++ wishlist/2-special/hand-cannons.py
@@ -14,15 +14,34 @@ class UncivilDiscourse(RollDefinition):
         ]
     rolls = [
         Roll(
+            'Super roll',
+            [barrel.ArrowheadBrake, AnyPerk],
+            [battery.IonizedHeatsink, AnyPerk],
+            [trait.AirTrigger],
+            [trait.LoneWolf],
+            [trait.CoolingBaubles],
+            [trait.JoltingFeedback],
+            [trait.PrecisionInstrument],
+            [trait.OpeningShot],
+            ),
+        Roll(
             """
-            Damage dealing with Lucky Pants.
+            Miniboss damage.
             Accelerated Assault with 130 Heat Gen and 100 Cooling Efficiency
             allows to shoot 14 rounds before overheating.
             With 120 Heat Gen it allows to shoot 15 instead.
             """,
             [barrel.ArrowheadBrake, AnyPerk],
-            [battery.EnhancedHeatsink, battery.IonizedHeatsink, AnyPerk],
-            [trait.AirTrigger, AnyPerk],
-            [trait.PrecisionInstrument],
+            [battery.IonizedHeatsink, AnyPerk],
+            [trait.AirTrigger, trait.CoolingBaubles],
+            [trait.JoltingFeedback, trait.PrecisionInstrument],
+            ),
+        Roll(
+            'PvP',
+            [barrel.ArrowheadBrake, AnyPerk],
+            [battery.IonizedHeatsink, AnyPerk],
+            [trait.LoneWolf],
+            [trait.JoltingFeedback],
+            [trait.OpeningShot],
             ),
         ]
