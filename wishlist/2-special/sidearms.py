aa3f7d30e44b83025a20d7bd6811dc2f9f569348
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2026-07-26 02:56:09 +0300

    [wishlist] Update rocket sidearms

--- wishlist/2-special/sidearms.py
+++ wishlist/2-special/sidearms.py
@@ -1,5 +1,8 @@
 from wishlist import *
 
+default_barrels = [launcher_barrel.VolatileLaunch, AnyPerk]
+default_mags = [magazine.TacticalMag, AnyPerk]
+
 
 class HighAlbedo(RollDefinition):
     """
@@ -9,6 +12,33 @@ class HighAlbedo(RollDefinition):
     https://destiny.report/w/2662459958
     """
     item = Item('High Albedo', hash=2662459958)
+    is_chosen = True
+    rolls = [
+        Roll(
+            'Super roll',
+            default_barrels,
+            default_mags,
+            [trait.AutoLoadingHolster],
+            [trait.ImpulseAmplifier],
+            [trait.BlastDistributor],
+            [trait.KineticTremors],
+            [trait.OneForAll],
+            ),
+        Roll(
+            'Ad clear',
+            default_barrels,
+            default_mags,
+            [trait.ImpulseAmplifier, trait.AutoLoadingHolster],
+            [trait.OneForAll, trait.KineticTremors],
+            ),
+        Roll(
+            'Blast Distributor',
+            default_barrels,
+            default_mags,
+            [trait.BlastDistributor],
+            [trait.OneForAll, trait.KineticTremors],
+            ),
+        ]
 
 
 class IndebtedKindness(RollDefinition):
@@ -19,6 +49,25 @@ class IndebtedKindness(RollDefinition):
     https://destiny.report/w/2554513694
     """
     item = Item('Indebted Kindness', hash=2554513694)
+    rolls = [
+        Roll(
+            'Super roll',
+            default_barrels,
+            default_mags,
+            [trait.ImpulseAmplifier],
+            [trait.Deconstruct],
+            [trait.AirTrigger],
+            [trait.GearShift],
+            [trait.ChainReaction],
+            ),
+        Roll(
+            'Ad clear',
+            default_barrels,
+            default_mags,
+            [trait.AirTrigger, trait.ImpulseAmplifier],
+            [trait.GearShift, trait.ChainReaction],
+            ),
+        ]
 
 
 class Unfall(RollDefinition):
@@ -29,20 +78,23 @@ class Unfall(RollDefinition):
     https://destiny.report/w/738446555
     """
     item = Item('Unfall', hash=738446555)
+    is_chosen = True
     rolls = [
         Roll(
-            'Ad clear',
-            [launcher_barrel.VolatileLaunch, AnyPerk],
-            [magazine.TacticalMag, AnyPerk],
+            'Super roll',
+            default_barrels,
+            default_mags,
             [trait.ImpulseAmplifier],
-            [trait.OneForAll, trait.JoltingFeedback],
+            [trait.JoltingFeedback],
+            [trait.Deconstruct],
+            [trait.OneForAll],
             ),
         Roll(
-            'Deconstruct',
-            [launcher_barrel.VolatileLaunch, AnyPerk],
-            [magazine.TacticalMag, AnyPerk],
+            'Ad clear',
+            default_barrels,
+            default_mags,
             [trait.ImpulseAmplifier],
-            [trait.Deconstruct],
+            [trait.OneForAll, trait.JoltingFeedback],
             ),
         ]
 
@@ -55,28 +107,54 @@ class AberrantAction(RollDefinition):
     https://destiny.report/w/2198166292
     """
     item = Item('Aberrant Action', hash=2198166292)
+    roll = Roll(
+        'Solar combo',
+        default_barrels,
+        default_mags,
+        [trait.HealClip],
+        [trait.Incandescent],
+        )
 
 
 class ReturnedMemory(RollDefinition):
     """
     Solar Sidearm, Micro-Missile Frame, Anti-Unstoppable
-    Source: Lord Shaxx; Tenet of Bravery    
+    Source: Lord Shaxx; Tenet of Bravery
     https://www.light.gg/db/items/4049127142
     https://destiny.report/w/4049127142
     """
     item = Item('Returned Memory', hash=4049127142)
+    is_chosen = True
     rolls = [
         Roll(
+            'Super roll',
+            default_barrels,
+            default_mags,
+            [trait.ImpromptuAmmunition],
+            [trait.HealClip],
+            [trait.BlastDistributor],
+            [trait.Incandescent],
+            [trait.OneForAll],
+            [trait.Redirection],
+            ),
+        Roll(
             """
-            PvE.
+            Ad clear.
             Note for Redirection: stacks are granted and consumed for every hit,
             i.e., 2 * (1 [impact] + #[targets hit by an explosion]).
             If a target dies to impact, explosion doesn't hit it.
             Multiplier 2 is replaced with 3 for consumption if perk is not enhanced.
             """,
-            [launcher_barrel.VolatileLaunch, AnyPerk],
-            [magazine.TacticalMag, AnyPerk],
-            [trait.HealClip, trait.ImpulseAmplifier],
+            default_barrels,
+            default_mags,
+            [trait.HealClip, trait.ImpromptuAmmunition],
+            [trait.Incandescent, trait.OneForAll, trait.Redirection],
+            ),
+        Roll(
+            'Blast Distributor',
+            default_barrels,
+            default_mags,
+            [trait.BlastDistributor],
             [trait.Incandescent, trait.OneForAll, trait.Redirection],
             ),
         ]
@@ -93,6 +171,34 @@ class LotusEater(RollDefinition):
         Item('Lotus-Eater', hash=924095500),
         Item('Lotus-Eater', hash=837298567),
         ]
+    is_chosen = True
+    rolls = [
+        Roll(
+            'Super roll',
+            default_barrels,
+            default_mags,
+            [trait.Reconstruction],
+            [trait.RepulsorBrace],
+            [trait.AirTrigger],
+            [trait.DestabilizingRounds],
+            [trait.CollectiveAction],
+            [trait.WitheringGaze],
+            ),
+        Roll(
+            'Ad clear',
+            default_barrels,
+            default_mags,
+            [trait.AirTrigger, trait.Reconstruction],
+            [trait.DestabilizingRounds],
+            ),
+        Roll(
+            'Void combo',
+            default_barrels,
+            default_mags,
+            [trait.RepulsorBrace],
+            [trait.DestabilizingRounds],
+            ),
+        ]
 
 
 class TinashasMastery(RollDefinition):
@@ -103,6 +209,25 @@ class TinashasMastery(RollDefinition):
     https://destiny.report/w/247984828
     """
     item = Item("Tinasha's Mastery", hash=247984828)
+    is_chosen = True
+    rolls = [
+        Roll(
+            'Super roll',
+            default_barrels,
+            default_mags,
+            [trait.AirTrigger],
+            [trait.Deconstruct],
+            [trait.ChillClip],
+            [trait.OneForAll],
+            ),
+        Roll(
+            'Ad clear',
+            default_barrels,
+            default_mags,
+            [trait.AirTrigger],
+            [trait.ChillClip, trait.OneForAll],
+            ),
+        ]
 
 
 class TheCall(RollDefinition):
@@ -113,3 +238,11 @@ class TheCall(RollDefinition):
     https://destiny.report/w/3947966653
     """
     item = Item('The Call', hash=3947966653)
+    is_chosen = True
+    roll = Roll(
+        'Ad clear',
+        default_barrels,
+        default_mags,
+        [trait.Reconstruction],
+        [trait.Hatchling],
+        )
