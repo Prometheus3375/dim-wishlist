0b5121bc3601c45ab038b669b5e5dd3ee567586f
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2026-07-26 22:03:22 +0300

    [wishlist] Update arc gls

--- wishlist/3-heavy/grenade-launchers/1-arc-gl.py
+++ wishlist/3-heavy/grenade-launchers/1-arc-gl.py
@@ -1,4 +1,5 @@
 from wishlist import *
+from . import *
 
 
 class Doomsday(RollDefinition):
@@ -9,16 +10,26 @@ class Doomsday(RollDefinition):
     https://destiny.report/w/713132408
     """
     item = Item('Doomsday', hash=713132408)
-
-
-class Hullabaloo(RollDefinition):
-    """
-    Arc Drum Grenade Launcher, Compressed Wave Frame, Anti-Unstoppable
-    Source: Guardian Games
-    https://www.light.gg/db/items/2449096504
-    https://destiny.report/w/2449096504
-    """
-    item = Item('Hullabaloo', hash=2449096504)
+    is_chosen = True
+    rolls = [
+        Roll(
+            'Super roll',
+            default_barrels,
+            default_mags,
+            [trait.AutoLoadingHolster],
+            [trait.SuperchargedMagazine],
+            [trait.EnviousArsenal],
+            [trait.BaitAndSwitch],
+            [trait.ExplosiveLight],
+            ),
+        Roll(
+            'Damage dealing',
+            default_barrels,
+            default_mags,
+            [trait.SuperchargedMagazine, trait.EnviousArsenal, trait.AutoLoadingHolster],
+            [trait.ExplosiveLight, trait.BaitAndSwitch],
+            ),
+        ]
 
 
 class Tarnation(RollDefinition):
@@ -29,6 +40,13 @@ class Tarnation(RollDefinition):
     https://destiny.report/w/2721157927
     """
     item = Item('Tarnation', hash=2721157927)
+    roll = Roll(
+        'Damage dealing',
+        default_barrels,
+        default_mags,
+        [trait.EnviousAssassin],
+        [trait.ExplosiveLight],
+        )
 
 
 class WendigoGL3(RollDefinition):
@@ -38,4 +56,17 @@ class WendigoGL3(RollDefinition):
     https://www.light.gg/db/items/4021098353
     https://destiny.report/w/4021098353
     """
-    item = Item('Wendigo GL3', hash=4021098353)
+    items = [
+        Item('Wendigo GL3', hash=4021098353),
+        Item('Wendigo GL3', hash=1854753404),
+        Item('Wendigo GL3 (Adept)', hash=3915197957),
+        Item('Wendigo GL3', hash=3183283212),
+        Item('Wendigo GL3 (Adept)', hash=555148853),
+        ]
+    roll = Roll(
+        'Damage dealing',
+        default_barrels,
+        default_mags,
+        [trait.AutoLoadingHolster],
+        [trait.ExplosiveLight],
+        )
