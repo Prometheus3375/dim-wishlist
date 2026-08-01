070c8b9f8d6bb1c3e6409c72a24c1c10c64d3f8f
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2026-08-02 01:55:40 +0300

    [wishlist] Update strand lmgs

--- wishlist/3-heavy/machine-guns/5-strand-lmg.py
+++ wishlist/3-heavy/machine-guns/5-strand-lmg.py
@@ -1,4 +1,5 @@
 from wishlist import *
+from . import *
 
 
 class CircularLogic(RollDefinition):
@@ -9,6 +10,31 @@ class CircularLogic(RollDefinition):
     https://destiny.report/w/2528793321
     """
     item = Item('Circular Logic', hash=2528793321)
+    rolls = [
+        Roll(
+            'Super roll',
+            default_barrels,
+            flared_mag,
+            [trait.AmbitiousAssassin],
+            [trait.Firefly],
+            [trait.MegaKillClip],
+            [trait.Hatchling],
+            ),
+        Roll(
+            'Ad clear',
+            default_barrels,
+            flared_mag,
+            [trait.AmbitiousAssassin],
+            [trait.MegaKillClip],
+            ),
+        Roll(
+            'Precision combo',
+            default_barrels,
+            flared_mag,
+            [trait.Firefly],
+            [trait.Hatchling],
+            ),
+        ]
 
 
 class DIABLERETS06(RollDefinition):
@@ -19,6 +45,40 @@ class DIABLERETS06(RollDefinition):
     https://destiny.report/w/1120206506
     """
     item = Item('DIABLERETS 06', hash=1120206506)
+    rolls = [
+        Roll(
+            'Super roll',
+            default_barrels,
+            appended_mag,
+            [trait.Hatchling],
+            [trait.Subsistence],
+            [trait.AttritionOrbs],
+            [trait.Meganeura],
+            [trait.Redirection],
+            [trait.KillingTally],
+            ),
+        Roll(
+            'Ad clear',
+            default_barrels,
+            appended_mag,
+            [trait.Hatchling],
+            [trait.Redirection],
+            ),
+        Roll(
+            'Ad clear',
+            default_barrels,
+            appended_mag,
+            [trait.Subsistence],
+            [trait.KillingTally],
+            ),
+        Roll(
+            'Precision combo',
+            default_barrels,
+            appended_mag,
+            [trait.Hatchling],
+            [trait.Meganeura],
+            ),
+        ]
 
 
 class ProMemoria(RollDefinition):
@@ -29,6 +89,13 @@ class ProMemoria(RollDefinition):
     https://destiny.report/w/3605603507
     """
     item = Item('Pro Memoria', hash=3605603507)
+    Roll(
+        'Precision combo',
+        default_barrels,
+        flared_mag,
+        [trait.Hatchling],
+        [trait.MegaKillClip],
+        )
 
 
 class QuaVinctusIV(RollDefinition):
@@ -44,10 +111,26 @@ class QuaVinctusIV(RollDefinition):
         ]
     rolls = [
         Roll(
+            'Super roll',
+            default_barrels,
+            appended_mag,
+            [trait.Hatchling],
+            [trait.Demolitionist],
+            [trait.Meganeura],
+            [trait.KillingTally],
+            ),
+        Roll(
+            'Precision combo',
+            default_barrels,
+            appended_mag,
+            [trait.Hatchling],
+            [trait.Meganeura],
+            ),
+        Roll(
             'Ad clear',
-            [barrel.ArrowheadBrake, AnyPerk],
-            [magazine.AppendedMag, AnyPerk],
-            [trait.Hatchling, trait.Demolitionist],
-            [trait.Meganeura, trait.KillingTally, trait.Tear],
+            default_barrels,
+            appended_mag,
+            [trait.Hatchling],
+            [trait.KillingTally],
             ),
         ]
