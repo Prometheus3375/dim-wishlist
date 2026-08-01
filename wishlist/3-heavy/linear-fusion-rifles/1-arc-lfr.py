9dec074f4365fb02c1396044fa7787c63a7cb436
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2026-08-02 00:17:49 +0300

    [wishlist] Update arc lfr

--- wishlist/3-heavy/linear-fusion-rifles/1-arc-lfr.py
+++ wishlist/3-heavy/linear-fusion-rifles/1-arc-lfr.py
@@ -1,4 +1,5 @@
 from wishlist import *
+from . import *
 
 
 class Boomslang4FR(RollDefinition):
@@ -9,13 +10,25 @@ class Boomslang4FR(RollDefinition):
     https://destiny.report/w/3926153598
     """
     item = Item('Boomslang-4FR', hash=3926153598)
-    roll = Roll(
-        'Damage dealing',
-        [barrel.ArrowheadBrake, AnyPerk],
-        [battery.AcceleratedCoils, AnyPerk],
-        [trait.EnviousArsenal, trait.RapidHit],
-        [trait.PrecisionInstrument],
-        )
+    rolls = [
+        Roll(
+            'Super roll',
+            default_barrels,
+            default_mags,
+            [trait.EnviousArsenal],
+            [trait.RapidHit],
+            [trait.PrecisionInstrument],
+            [origin.VeistStinger],
+            ),
+        Roll(
+            'Damage dealing',
+            default_barrels,
+            default_mags,
+            [trait.EnviousArsenal, trait.RapidHit],
+            [trait.PrecisionInstrument],
+            [origin.VeistStinger],
+            ),
+        ]
 
 
 class SailspyPitchglass(RollDefinition):
@@ -36,6 +49,25 @@ class Stormchaser(RollDefinition):
     https://destiny.report/w/2862666249
     """
     item = Item('Stormchaser', hash=2862666249)
+    rolls = [
+        Roll(
+            'Super roll',
+            default_barrels,
+            default_mags,
+            [trait.AutoLoadingHolster],
+            [trait.SuperchargedMagazine],
+            [trait.JoltingFeedback],
+            [trait.GearShift],
+            [trait.AggregateCharge],
+            ),
+        Roll(
+            'Damage dealing',
+            default_barrels,
+            default_mags,
+            [trait.JoltingFeedback, trait.SuperchargedMagazine, trait.AutoLoadingHolster],
+            [trait.AggregateCharge, trait.GearShift],
+            ),
+        ]
 
 
 class WillfulHamartia(RollDefinition):
@@ -46,3 +78,40 @@ class WillfulHamartia(RollDefinition):
     https://destiny.report/w/1952295804
     """
     item = Item('Willful Hamartia', hash=1952295804)
+    rolls = [
+        Roll(
+            'Super roll',
+            default_barrels,
+            default_mags,
+            [trait.AutoLoadingHolster],
+            [trait.SuperchargedMagazine],
+            [trait.JoltingFeedback],
+            [trait.GearShift],
+            [trait.AggregateCharge],
+            ),
+        Roll(
+            'Damage dealing',
+            default_barrels,
+            default_mags,
+            [trait.JoltingFeedback, trait.SuperchargedMagazine, trait.AutoLoadingHolster],
+            [trait.AggregateCharge, trait.GearShift],
+            ),
+        ]
+
+
+class LineInTheSand(RollDefinition):
+    """
+    Arc Linear Fusion Rifle, Precision Frame, Anti-Barrier, Legacy
+    Source: Xûr
+    https://www.light.gg/db/items/2450049485
+    https://destiny.report/w/2450049485
+    """
+    item = Item('Line in the Sand', hash=2450049485)
+    is_chosen = True
+    roll = Roll(
+        'Damage dealing',
+        default_barrels,
+        [battery.EnhancedBattery, AnyPerk],
+        [trait.ClownCartridge, trait.Deconstruct],
+        [trait.BaitAndSwitch],
+        )
---
+++ wishlist/3-heavy/linear-fusion-rifles/__init__.py
@@ -0,0 +1,6 @@
+__all__ = 'default_barrels', 'default_mags'
+
+from wishlist import AnyPerk, barrel, battery
+
+default_barrels = [barrel.ArrowheadBrake, AnyPerk]
+default_mags = [battery.AcceleratedCoils, AnyPerk]
